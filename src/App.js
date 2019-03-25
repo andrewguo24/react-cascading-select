53135d0d54aa618ba32930292c91afc703202738
Ge Guo <alex@Ges-MacBook-Pro.local>
2019-03-25 17:41:28 +1100

    updated

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
-class App extends Component {
+export default class App extends React.Component {
+  state = {
+    selections: [],
+    selectedId: "0"
+  };
+
+  componentDidMount = () => {
+    fetch("./selections.json")
+      .then(res => res.json())
+      .then(data => this.setState({ selections: data.selections }));
+  };
+
+  handleChange = e => {
+    this.setState({ selectedId: e.target.value });
+  };
+
   render() {
+    const { selections, selectedId } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <select onChange={this.handleChange} className="primary-select">
+          {selections.map(option => (
+            <option key={option.id} value={option.id}>
+              {option.primary}
+            </option>
+          ))}
+        </select>
+        <select>
+          {selections.map(
+            item =>
+              selectedId === item.id &&
+              item.subSelections.map(sub => (
+                <option key={sub.id}>{sub.label}</option>
+              ))
+          )}
+        </select>
+      </>
     );
   }
 }
-
-export default App;
